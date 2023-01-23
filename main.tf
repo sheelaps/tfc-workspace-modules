34e5e70867ac4effec2bb9a4172069d7cdc45cf7
sheelaps <123383883+sheelaps@users.noreply.github.com>
2023-01-23 21:36:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ variable "subnet_address_prefixes" {
 
 module "networking" {
   source  = "app.terraform.io/testterraformorg1/networking/azurerm"
-  version = "1.1.2"
+  version = "1.1.3"
 
   name                    = var.name
   location                = var.location
