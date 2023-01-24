639efe0e371b74ee3d0dca612fac47e06fe78e54
sheelaps <123383883+sheelaps@users.noreply.github.com>
2023-01-24 12:38:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,5 +22,4 @@ module "networking" {
   name                    = var.name
   location                = var.location
   vnet_address_spacing    = var.vnet_address_spacing
-  subnet_address_prefixes = [ "10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24" ]
-}
+  subnet_address_prefixes = var.subnet_address_prefixes
